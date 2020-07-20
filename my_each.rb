f28e99a347f2d6bc4ff57d87eb1b7bbd04180320
Jason Keeling <s2jason@yahoo.com>
2020-07-20 23:10:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
+  i = 0
+  
+  while i < array.length
+    yield(array[i])
+    
+  end
+  array
   # code here
+end
+
+my_each(['hi','hello','bye','goodbye']) do |i|
+  puts i
 end
